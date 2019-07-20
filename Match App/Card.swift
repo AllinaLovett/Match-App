9f7d565b59c44a275b32a6f1f512d7ccb774be1c
AllinaLovett <allina.lovett@gmail.com>
2019-07-19 21:46:26 -0700

    Initial Commit

---
+++ Match App/Card.swift
@@ -0,0 +1,17 @@
+//
+//  Card.swift
+//  Match App
+//
+//  Created by MacBook Pro  on 7/18/19.
+//  Copyright © 2019 MacBook Pro . All rights reserved.
+//
+
+import Foundation
+
+
+class Card {
+    
+    var imageName = ""
+    var isFlipped = false
+    var isMatched = false
+}
